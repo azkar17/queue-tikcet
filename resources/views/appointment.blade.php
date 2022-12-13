95b2817a8b7b181872735dd97bde3b7ac5f4d0c2
Azkar17 <azkarabidi17@gmail.com>
2022-12-14 02:33:19 +0800

    Initial commit

---
+++ resources/views/appointment.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Patient</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="mt-2">
+            <h2>New Appointment Register  {{$title}}</h2>
+        </div>
+        <div class="d-flex">
+            <table class="table">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">January</th>
+                        <th scope="col">Febuary</th>
+                        <th scope="col">March</th>
+                        <th scope="col">April</th>
+                        <th scope="col">Mei</th>
+                        <th scope="col">Jun</th>
+                        <th scope="col">July</th>
+                        <th scope="col">August</th>
+                        <th scope="col">September</th>
+                        <th scope="col">October</th>
+                        <th scope="col">November</th>
+                        <th scope="col">December</th>
+                      </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                <td>{{'Month'}}</td>    
+                <td>{{$jan}}</td>    
+                <td>{{$feb}}</td>    
+                <td>{{$marc}}</td>    
+                <td>{{$april}}</td>    
+                <td>{{$mei}}</td>    
+                <td>{{$jun}}</td>    
+                <td>{{$july}}</td>    
+                <td>{{$augt}}</td>    
+                <td>{{$sep}}</td>    
+                <td>{{$oct}}</td>    
+                <td>{{$nov}}</td>    
+                <td>{{$dec}}</td>    
+                </tr>            
+              </tbody>
+              </table>
+
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,299 @@
+<?php
+
+use App\Models\Appointment;
+use App\Models\Patient;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/patients', function () {
+    // return view('welcome');
+$title ='Setapak';
+    $jan=DB::table('patients')
+            ->whereMonth('created_at', '01')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $feb=DB::table('patients')
+            ->whereMonth('created_at', '02')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $marc=DB::table('patients')
+            ->whereMonth('created_at', '03')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $april=DB::table('patients')
+            ->whereMonth('created_at', '04')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $mei=DB::table('patients')
+            ->whereMonth('created_at', '05')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $jun=DB::table('patients')
+            ->whereMonth('created_at', '06')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $july=DB::table('patients')
+            ->whereMonth('created_at', '07')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $augt=DB::table('patients')
+            ->whereMonth('created_at', '08')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $sep=DB::table('patients')
+            ->whereMonth('created_at', '09')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $oct=DB::table('patients')
+            ->whereMonth('created_at', '10')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $nov=DB::table('patients')
+            ->whereMonth('created_at', '11')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $dec=DB::table('patients')
+            ->whereMonth('created_at', '12')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+
+            // dd(compact('jan','feb','marc','april','mei','june','july','aug','sep','oct','nov','dec'));
+            return view('patient',compact('jan','feb','marc','april','mei','jun','july','augt','sep','oct','nov','dec','title'));
+});
+Route::get('/appointment', function () {
+   
+    $title ='Setapak';
+    $jan=DB::table('appointments')
+            ->whereMonth('created_at', '01')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $feb=DB::table('appointments')
+            ->whereMonth('created_at', '02')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $marc=DB::table('appointments')
+            ->whereMonth('created_at', '03')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $april=DB::table('appointments')
+            ->whereMonth('created_at', '04')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $mei=DB::table('appointments')
+            ->whereMonth('created_at', '05')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $jun=DB::table('appointments')
+            ->whereMonth('created_at', '06')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $july=DB::table('appointments')
+            ->whereMonth('created_at', '07')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $augt=DB::table('appointments')
+            ->whereMonth('created_at', '08')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $sep=DB::table('appointments')
+            ->whereMonth('created_at', '09')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $oct=DB::table('appointments')
+            ->whereMonth('created_at', '10')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $nov=DB::table('appointments')
+            ->whereMonth('created_at', '11')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $dec=DB::table('appointments')
+            ->whereMonth('created_at', '12')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+
+            return view('appointment',compact('jan','feb','marc','april','mei','jun','july','augt','sep','oct','nov','dec','title'));
+});
+
+
+Route::get('/ca',function(){
+// Check if an item with the key 'key' exists in the cache
+if (Cache::has('patient-jan')) {
+    // Get the cached value
+    $value = Cache::get('patient-jan');
+
+    // Return a response with the cached value
+    return response($value);
+} else {
+    // Return a 404 not found error
+    return response()->json(['error' => 'Item not found'], 404);
+}
+});
+Route::get('/p',function(){
+    // $title ='Setapak';
+    $data=Cache::get('patient-jan',100,function(){
+        return Patient::whereMonth('created_at', '01')
+        ->whereYear('created_at', '2022')
+        ->get()->count();
+    });
+    return response()->json(compact('data'));
+    // $jan=Patient::whereMonth('created_at', '01')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $feb=Patient::whereMonth('created_at', '02')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $marc=Patient::whereMonth('created_at', '03')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $april=Patient::whereMonth('created_at', '04')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $mei=Patient::whereMonth('created_at', '05')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $jun=Patient::whereMonth('created_at', '06')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $july=Patient::whereMonth('created_at', '07')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $augt=Patient::whereMonth('created_at', '08')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $sep=Patient::whereMonth('created_at', '09')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $oct=Patient::whereMonth('created_at', '10')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $nov=Patient::whereMonth('created_at', '11')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+    // $dec=Patient::whereMonth('created_at', '12')
+    //         ->whereYear('created_at', '2022')
+    //         ->get()->count();
+
+
+            // dd(compact('jan','feb','marc','april','mei','june','july','aug','sep','oct','nov','dec'));
+            // return response()->json(compact('jan','feb','marc','april','mei','jun','july','augt','sep','oct','nov','dec','title'));
+});
+Route::get('/p1', function () {
+    // return view('welcome');
+$title ='Setapak';
+    $jan=DB::table('patients')
+            ->whereMonth('created_at', '01')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $feb=DB::table('patients')
+            ->whereMonth('created_at', '02')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $marc=DB::table('patients')
+            ->whereMonth('created_at', '03')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $april=DB::table('patients')
+            ->whereMonth('created_at', '04')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $mei=DB::table('patients')
+            ->whereMonth('created_at', '05')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $jun=DB::table('patients')
+            ->whereMonth('created_at', '06')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $july=DB::table('patients')
+            ->whereMonth('created_at', '07')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $augt=DB::table('patients')
+            ->whereMonth('created_at', '08')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $sep=DB::table('patients')
+            ->whereMonth('created_at', '09')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $oct=DB::table('patients')
+            ->whereMonth('created_at', '10')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $nov=DB::table('patients')
+            ->whereMonth('created_at', '11')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+    $dec=DB::table('patients')
+            ->whereMonth('created_at', '12')
+            ->whereYear('created_at', '2022')
+            ->get()->count();
+
+
+            return response()->json(compact('jan','feb','marc','april','mei','jun','july','augt','sep','oct','nov','dec','title'));
+});
+Route::get('/test',function(){
+        $appointment = Cache::remember('app', 20, function () {
+                return Appointment::first();
+            });
+            return response()->json($appointment);
+});
